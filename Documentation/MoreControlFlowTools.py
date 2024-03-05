51b456270f2653694df58b12bb3d244f6ab7c52b
xionkq <3030199850@qq.com>
2024-03-05 18:15:13 +0800

    update

--- Documentation/MoreControlFlowTools.py
+++ Documentation/MoreControlFlowTools.py
@@ -1,4 +1,4 @@
-# if语句、input输入
+# 1.if语句、input输入
 x = int(input("Please enter an integer: "))
 
 if x < 0:
@@ -10,3 +10,21 @@ elif x == 1:
     print('Single')
 else:
     print('More')
+
+
+# 2.for语句迭代对象
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+for user, status in users.copy().items():
+    if status == 'inactive':
+        print(users[user])
+
+
+# 3.range内置函数
+print(list(range(5, 10)))
+print(list(range(0, 10, 3)))
+print(list(range(-10, -100, -30)))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
