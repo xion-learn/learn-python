8a138d9a45c06b91cbf3f2e7e041005a0d1e27de
xionkq <3030199850@qq.com>
2024-03-07 22:16:19 +0800

    update

--- Documentation/MoreControlFlowTools.py
+++ Documentation/MoreControlFlowTools.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # 1.if语句、input输入
 x = int(input("Please enter an integer: "))
 
@@ -11,14 +13,12 @@ elif x == 1:
 else:
     print('More')
 
-
 # 2.for语句迭代对象
 users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
 
 for user, status in users.copy().items():
     if status == 'inactive':
         print(users[user])
-
 
 # 3.range内置函数
 print(list(range(5, 10)))
@@ -29,7 +29,6 @@ a = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(a)):
     print(i, a[i])
 
-
 # 4.break和continue
 for n in range(2, 10):
     for x in range(2, n):
@@ -38,7 +37,36 @@ for n in range(2, 10):
     else:
         print(n, 'is a prime number')
 
-
 # 5.pass
 for n in range(2, 10):
     pass
+
+# 6.match语句
+point = (1, 1)
+match point:
+    case (1, 1) if False:
+        print("Origin")
+    case (0, y):
+        print(f"Y={y}")
+    case (x, 0):
+        print(f"X={x}")
+    case (x, y):
+        print(f"X={x}, Y={y}")
+    case _:
+        raise ValueError("Not a point")
+
+
+class Color(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+
+color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
