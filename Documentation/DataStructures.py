f0fbe9ab203fc52e737d41da1ba1b7f2b3718c92
xionkq <3030199850@qq.com>
2024-03-14 17:59:10 +0800

    update

--- Documentation/DataStructures.py
+++ Documentation/DataStructures.py
@@ -40,3 +40,39 @@ del a[:]
 print(a)
 
 del a
+
+
+# 元组
+t = 12345, 54321, 'hello!'
+print(t[0])
+
+# 元组可嵌套
+u = t, (1, 2, 3, 4, 5)
+print(t[0])
+
+# 元组元素是不可变的
+# t[0] = 88888
+
+# 元组可嵌套列表，列表可变
+v = ([1, 2, 3], [3, 2, 1])
+
+
+# 集合Set
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+print('orange' in basket, 'crabgrass' in basket)
+
+# 支持交并补等操作
+a = set('abracadabra')
+b = set('alacazam')
+print(a - b, a | b, a & b, a ^ b)
+
+
+# 字典
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+
+del tel['sape']
+tel['irv'] = 4127
+print('guido' in tel, 'jack' not in tel)
