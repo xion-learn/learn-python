dfb449c73661aa99fff907adc4ced9d66f0ba807
xionkq <3030199850@qq.com>
2024-03-19 17:59:46 +0800

    update

--- Documentation/Modules.py
+++ Documentation/Modules.py
@@ -1,3 +1,16 @@
 import module.fibo as fibo
+# fibo.fib(10)
 
-fibo.fib(10)
+# # 将模块名称引入到局部命名空间中
+# from module.fibo import fib, fib2
+# fib(500)
+
+
+# # 将模块中所有不以_开头的名称引入到局部命名空间中
+# from module.fibo import *
+# fib(500)
+
+
+# 一个模块只会导入一次，可通过特殊函数重新导入模块
+import importlib
+importlib.reload(fibo)
