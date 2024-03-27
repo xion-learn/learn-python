52fa163dc80e109592a09c61ba9364b7b25db999
xionkq <3030199850@qq.com>
2024-03-27 18:00:58 +0800

    add Classes

--- Documentation/Classes.py
+++ Documentation/Classes.py
@@ -22,3 +22,27 @@ def scope_test():
 
 scope_test()
 print("In global scope:", spam)
+
+
+# 定义类
+class MyClass:
+    """A simple example class"""
+    def __init__(self, some):
+        self.some = some
+
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+
+print(MyClass.i)
+my_class = MyClass('some')
+print(my_class.some)
+
+my_class_1 = MyClass('1')
+my_class_2 = MyClass('2')
+my_class_1.i = 'i1'
+my_class_2.i = 'i2'
+print(my_class_1.i)
+print(my_class_2.i)
